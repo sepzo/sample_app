fea623c40d562e58fb8c0884c5ade6fa131f08c3
Sushil Sharma <sushil342@gmail.com>
2013-10-30 09:16:01 +0530

    changed secret token file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
-  ('x' * 30) # meets minimum requirement of 30 chars long
-else
-  ENV['SECRET_TOKEN']
-end
+SampleApp::Application.config.secret_token = '1be0afcc8b6b8960abfd7520200dc3cd1c56175be6c7999a7be49d36075496907bdd32980b0ab25759c27ebd412961655acbd964f9184f950bc4de7278dceac0'
